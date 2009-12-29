5247dfdf467971f2304c70fde3599977d0436a12
jaideep.dhok <jaideep.dhok@gmail.com>
2009-12-29 05:22:29 +0000

    Reformatted source files according to Hadoop conventions

--- src/org/apache/hadoop/mapred/JobStatistics.java
+++ src/org/apache/hadoop/mapred/JobStatistics.java
@@ -1,6 +1,7 @@
 package org.apache.hadoop.mapred;
 
 public class JobStatistics {
+
   double cpu;
   double disk;
   double net;
@@ -9,17 +10,17 @@ public class JobStatistics {
   public JobStatistics(String statStr) {
     LearningScheduler.LOG.info("New job stat string:" + statStr);
     String toks[] = statStr.split(":");
-  
+
     cpu = Double.parseDouble(toks[0]);
     disk = Double.parseDouble(toks[1]);
     net = Double.parseDouble(toks[2]);
     memory = Double.parseDouble(toks[3]);
-  }    
+  }
 
   public String toString() {
-    return ""+ cpu + "\t" + disk + "\t" + net + "\t" + memory;
+    return "" + cpu + "\t" + disk + "\t" + net + "\t" + memory;
   }
-  
+
   public static void main(String[] args) {
     String stat = "5:5:5:5";
     JobStatistics jobstat = new JobStatistics(stat);
--- src/org/apache/hadoop/mapred/NodeEnvironment.java
+++ src/org/apache/hadoop/mapred/NodeEnvironment.java
@@ -1,12 +1,11 @@
 package org.apache.hadoop.mapred;
 
-
 class NodeEnvironment {
 
   double loadAverage;
-  int memUsed, swapUsed;  
-  int numMaps, numReduces, numCpus, memTotal, swapTotal, cpuFreq; 
-  String trackerName; 
+  int memUsed, swapUsed;
+  int numMaps, numReduces, numCpus, memTotal, swapTotal, cpuFreq;
+  String trackerName;
   public static final int loadScaleFactor = 10;
   double diskio, netio, ucpu;
   public static final double UCPU_LIMIT = 95;
@@ -15,27 +14,28 @@ class NodeEnvironment {
 
   NodeEnvironment(TaskTrackerStatus ttstatus, LearningScheduler sched) {
     TaskTrackerStatus.ResourceStatus resources = ttstatus.getResourceStatus();
-    loadAverage = resources.getLoadAverage();    
+    loadAverage = resources.getLoadAverage();
     numMaps = ttstatus.countMapTasks();
     numReduces = ttstatus.countReduceTasks();
     numCpus = resources.getNumCpus();
     memTotal = resources.getMemTotal();
-    memUsed = memTotal - (int)resources.getMemFree();
+    memUsed = memTotal - (int) resources.getMemFree();
     swapTotal = resources.getSwapTotal();
-    swapUsed = swapTotal - (int)resources.getSwapFree();
+    swapUsed = swapTotal - (int) resources.getSwapFree();
     cpuFreq = resources.getCpuFreq();
     trackerName = ttstatus.getTrackerName();
     netio = resources.getNetworkIO();
     diskio = resources.getDiskIO();
     ucpu = resources.getUserCpu();
   }
-  
-  public NodeEnvironment() {}
+
+  public NodeEnvironment() {
+  }
 
   public double getLoadAverage() {
-      return loadAverage;
+    return loadAverage;
   }
-  
+
   public boolean underLoaded(double procsPerCpu) {
     return !overLoaded(procsPerCpu, true);
   }
@@ -43,34 +43,45 @@ class NodeEnvironment {
   public String toString() {
     // use string builder
     StringBuilder builder = new StringBuilder();
-    builder.append(loadAverage); builder.append('\t');
-    builder.append(this.numMaps); builder.append('\t');
-    builder.append(this.numReduces); builder.append('\t');
-    builder.append(this.memUsed); builder.append('\t');
-    builder.append(this.swapUsed); builder.append('\t');
-    builder.append(this.diskio); builder.append('\t');
-    builder.append(this.netio); builder.append('\t');
-    builder.append(this.ucpu); builder.append('\t');
-    builder.append(this.numCpus); builder.append('\t');
-    builder.append(this.cpuFreq); builder.append('\t');
-    builder.append(this.memTotal); builder.append('\t');
-    builder.append(this.swapTotal); builder.append('\t');
+    builder.append(loadAverage);
+    builder.append('\t');
+    builder.append(this.numMaps);
+    builder.append('\t');
+    builder.append(this.numReduces);
+    builder.append('\t');
+    builder.append(this.memUsed);
+    builder.append('\t');
+    builder.append(this.swapUsed);
+    builder.append('\t');
+    builder.append(this.diskio);
+    builder.append('\t');
+    builder.append(this.netio);
+    builder.append('\t');
+    builder.append(this.ucpu);
+    builder.append('\t');
+    builder.append(this.numCpus);
+    builder.append('\t');
+    builder.append(this.cpuFreq);
+    builder.append('\t');
+    builder.append(this.memTotal);
+    builder.append('\t');
+    builder.append(this.swapTotal);
+    builder.append('\t');
     return builder.toString();
   }
-  
 
   public boolean overLoaded(double procsPerCpu, boolean multipleResources) {
     if (multipleResources) {
-      return multipleResourcesOverloaded(procsPerCpu);  
+      return multipleResourcesOverloaded(procsPerCpu);
     } else {
-      return Math.ceil(loadAverage * loadScaleFactor) > 
-                 (procsPerCpu * numCpus * loadScaleFactor);
+      return Math.ceil(loadAverage * loadScaleFactor) >
+              (procsPerCpu * numCpus * loadScaleFactor);
     }
   }
 
   public boolean multipleResourcesOverloaded(double procsPerCpu) {
     boolean loadAvgOverload = Math.ceil(loadAverage) > (procsPerCpu * numCpus);
-    boolean ucpuOverload  = ucpu > UCPU_LIMIT;
+    boolean ucpuOverload = ucpu > UCPU_LIMIT;
     boolean netioOverload = netio > NETIO_LIMIT;
     boolean diskioOverload = diskio > DISKIO_LIMIT;
     return loadAvgOverload && ucpuOverload && netioOverload && diskioOverload;
